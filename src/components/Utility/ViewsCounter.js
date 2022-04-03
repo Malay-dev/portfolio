ba57c6bb217fe35445d178ccc9e14592b03cf83a
Malay <malaykumar2003@gmail.com>
2022-04-03 10:09:14 +0530

    commit

--- src/components/Utility/ViewsCounter.js
+++ src/components/Utility/ViewsCounter.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import axios from "axios";
 import { dbr } from "./firebase";
-import { onValue, ref, set, update } from "firebase/database";
+import { onValue, ref, set } from "firebase/database";
 import styled from "styled-components";
 const P1 = styled.p`
   font-size: 2rem;
